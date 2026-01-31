7104d83a03da75a6ba6cee6a8d866772b9d084c1
vivekgupta112204-lgtm <vivekgupta112204@gmail.com>
2026-01-31 17:04:13 +0530

    refactor: Remove React boilerplate and use plain HTML/JS
    The application has been refactored to use plain HTML and JavaScript for the main interface, removing the React setup. This simplifies the project structure and dependencies.

--- index.tsx
+++ index.tsx
@@ -1,15 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This file is no longer used. The app has been converted to pure HTML/JS in index.html.
+console.log("App running in pure HTML mode.");
